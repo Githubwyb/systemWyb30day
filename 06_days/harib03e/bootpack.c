1bb14f80bd0072cecd38aaccfc4f1bd56f9c25ff
wangyubo <1061322005@qq.com>
2022-10-16 23:52:45 +0800

    移植linux的kfifo到工程

---
+++ 06_days/harib03e/bootpack.c
@@ -0,0 +1,31 @@
+#include "bootpack.h"
+
+#include <stdio.h>
+
+#include "graphic.h"
+#include "naskfunc.h"
+
+void HariMain(void) {
+    struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
+
+    // 初始化段和中断
+    init_gdtidt();
+    init_pic();
+    io_sti();   // IDT和PIC初始化完成后，解除中断禁止
+
+    init_palette();
+    init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
+
+    unsigned char mcursor[256];
+    int mx = (binfo->scrnx - 16) / 2;
+    int my = (binfo->scrny - 28 - 16) / 2;
+    init_mouse_cursor8(mcursor, COL8_009999);
+    putblock8_8(binfo->vram, binfo->scrnx, 16, 16, mx, my, mcursor, 16);
+
+    io_out8(PIC0_IMR, 0xf9); // 11111001 允许PIC1和键盘的中断
+    io_out8(PIC1_IMR, 0xef); // 11101111 允许鼠标的中断
+
+    for (;;) {
+        io_hlt();
+    }
+}
---
+++ 07_days/harib04e/int.c
@@ -0,0 +1,74 @@
+#include <linux/kfifo.h>
+#include <stddef.h>
+#include <stdio.h>
+
+#include "bootpack.h"
+#include "fonts.h"
+#include "graphic.h"
+#include "naskfunc.h"
+
+void init_pic() {
+    io_out8(PIC0_IMR, 0xff);  // 禁止所有中断
+    io_out8(PIC1_IMR, 0xff);  // 禁止所有中断
+
+    io_out8(PIC0_ICW1, 0x11);    // 边沿触发模式
+    io_out8(PIC0_ICW2, 0x20);    // IRQ0-7由INT20-27接收
+    io_out8(PIC0_ICW3, 1 << 2);  // PIC1由IRQ2连接
+    io_out8(PIC0_ICW4, 0x01);    // 无缓冲区模式
+
+    io_out8(PIC1_ICW1, 0x11);    // 边沿触发模式
+    io_out8(PIC1_ICW2, 0x28);    // IRQ8-15由INT28-2f接收
+    io_out8(PIC1_ICW3, 1 << 1);  // PIC1由IRQ2连接
+    io_out8(PIC1_ICW4, 0x01);    // 无缓冲区模式
+
+    io_out8(PIC0_IMR, 0xfb);  // 11111011 PIC1以外全部禁止
+    io_out8(PIC1_IMR, 0xff);  // 禁止所有中断
+}
+
+#define PORT_KEYDAT 0x0060
+
+KeyBufType g_keybuf = (typeof(g_keybuf)){{{
+    .in = 0,
+    .out = 0,
+    .mask = __is_kfifo_ptr(&(g_keybuf)) ? 0 : ARRAY_SIZE((g_keybuf).buf) - 1,
+    .esize = sizeof(*(g_keybuf).buf),
+    .data = __is_kfifo_ptr(&(g_keybuf)) ? NULL : (g_keybuf).buf,
+}}};
+
+/**
+ * @brief 处理PS/2键盘的中断
+ *
+ * @param esp
+ */
+void inthandler21(int *esp) {
+    io_out8(PIC0_OCW2, 0x61);  // 通知PIC的IRQ_01已经处理完毕
+
+    unsigned char data = io_in8(PORT_KEYDAT);  // 读取键盘输入的值
+    kfifo_put(&g_keybuf, data);
+}
+
+/**
+ * @brief 处理PS/2鼠标的中断
+ *
+ * @param esp
+ */
+void inthandler2c(int *esp) {
+    struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
+
+    boxfill8(binfo->vram, binfo->scrnx, COL8_000000, 0, 0, 32 * 8 - 1, 15);
+    put_font8_str(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, "INT 2C (IRQ-12): PS/2 mouse");
+    for (;;) {
+        io_hlt();
+    }
+}
+
+/**
+ * @brief 处理27中断，athlon64x2机等芯片组的原因，在PIC初始化时会发生一次中断
+ *        此中断什么都不用做就好，一般是由于PIC初始化的电噪声引起的
+ *
+ * @param esp
+ */
+void inthandler27(int *esp) {
+    io_out8(PIC0_OCW2, 0x67);  // 向PIC通知IRQ-07处理完成
+    return;
+}
